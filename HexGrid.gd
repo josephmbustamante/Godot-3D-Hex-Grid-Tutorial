000d3c8c2f2d49c9172b691d36c096d762f03752
Scythia Marrow <scythia@marrow.science>
2023-09-07 18:18:19 -0600

    Upgrade to godot4, cleaned .import files and put them in .gitignore

--- HexGrid.gd
+++ HexGrid.gd
@@ -1,4 +1,4 @@
-extends Spatial
+extends Node3D
 
 
 const TILE_MATERIALS = [
@@ -11,7 +11,7 @@ const TILE_MATERIALS = [
 const TILE_SIZE := 1.0
 const HEX_TILE = preload("res://HexTile.tscn")
 
-export (int, 2, 20) var grid_size := 10
+var grid_size := 10
 
 
 func _ready() -> void:
@@ -22,10 +22,10 @@ func _generate_grid():
 	var tile_index := 0
 	for x in range(grid_size):
 		var tile_coordinates := Vector2.ZERO
-		tile_coordinates.x = x * TILE_SIZE * cos(deg2rad(30))
+		tile_coordinates.x = x * TILE_SIZE * cos(deg_to_rad(30))
 		tile_coordinates.y = 0 if x % 2 == 0 else TILE_SIZE / 2
 		for y in range(grid_size):
-			var tile = HEX_TILE.instance()
+			var tile = HEX_TILE.instantiate()
 			add_child(tile)
 			tile.translate(Vector3(tile_coordinates.x, 0, tile_coordinates.y))
 			tile_coordinates.y += TILE_SIZE
